a7de0795b56eeb362dfdf6a3f08e27d666bbb59b
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-06-13 00:31:42 +0200

    functionality

--- back/index.js
+++ back/index.js
@@ -2,6 +2,143 @@ import fs from "fs";
 import scrapeReddits from "./scrape.js";
 import express from "express";
 
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const sqlite3 = require("sqlite3").verbose();
+
+//connecting to db
+const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err.message);
+});
+
+function createTable(tableName, params) {
+  let sql = `CREATE TABLE ${tableName}(id INTEGER PRIMARY KEY, ${params})`;
+  db.run(sql);
+}
+// createTable("users", ["username", "pin", "doing", "done"]);
+// createTable("posts", ["pid", "subr", "title", "text", "status", "user"]);
+function dropTable(tableName) {
+  db.run(`DROP TABLE ${tableName}`);
+}
+// dropTable("posts");
+
+function addUser(username, pin) {
+  let sql = `SELECT * FROM users`;
+  db.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    if (rows.length == 0) {
+      sql = `INSERT INTO users(username, pin, doing, done) VALUES (?,?,0,0)`;
+      db.run(sql, [username, pin], (err) => {
+        if (err) return console.error(err.message);
+      });
+      return;
+    }
+
+    let occupied = false;
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].username === username) {
+        occupied = true;
+        break;
+      }
+    }
+    if (!occupied) {
+      sql = `INSERT INTO users(username, pin, doing, done) VALUES (?,?,0,0)`;
+      db.run(sql, [username, pin], (err) => {
+        if (err) return console.error(err.message);
+      });
+    } else {
+      console.log("OCCUPIED");
+    }
+  });
+}
+// addUser("stil", "2115");
+
+function addPost(pid, subr, title, text) {
+  let sql = `SELECT * FROM posts WHERE pid=?`;
+  db.all(sql, [pid], (err, rows) => {
+    if (err) return console.error(err.message);
+    console.log(rows);
+    if (rows.length == 0) {
+      let sql = `INSERT INTO posts(pid, subr, title, text, status) VALUES (?,?,?,?, 0)`;
+      db.run(sql, [pid, subr, title, text], (err) => {
+        if (err) return console.error(err.message);
+      });
+      return;
+    }
+    let exists = false;
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].subr === subr) {
+        exists = true;
+      }
+    }
+    if (!exists) {
+      let sql = `INSERT INTO posts(pid, subr, title, text, status) VALUES (?,?,?,?, 0)`;
+      db.run(sql, [pid, subr, title, text], (err) => {
+        if (err) return console.error(err.message);
+      });
+      return;
+    } else {
+      console.log("ALREADY EXISTS");
+    }
+  });
+}
+// addPost("asdd", "asdd", "Asdf", "Asdf");
+
+function updatePost(what, new_, pid, subr) {
+  let sql = `UPDATE posts SET ${what} = ? WHERE pid = ? AND subr = ?`;
+  db.run(sql, [new_, pid, subr], (err) => {
+    if (err) return console.error(err.message);
+  });
+}
+// updatePost("title", "amazing story", "asdd", "asdf");
+
+function updateUser(what, new_, name) {
+  let sql = `UPDATE users SET ${what} = ? WHERE username = ?`;
+  db.run(sql, [new_, name], (err) => {
+    if (err) return console.error(err.message);
+  });
+}
+// updateUser("pin", "0001", "admin");
+
+function readPosts(status, callback) {
+  let sql = `SELECT * FROM posts WHERE status=?`;
+  db.all(sql, [status], (err, rows) => {
+    if (err) return console.error(err.message);
+    callback(rows);
+  });
+}
+// readPosts();
+
+function readPicked(user, callback) {
+  let sql = `SELECT * FROM posts WHERE status=4 AND user=?`;
+  db.all(sql, [user], (err, rows) => {
+    if (err) return console.error(err.message);
+    callback(rows);
+  });
+}
+
+function getUser(username, callback) {
+  let sql = `SELECT * FROM users WHERE username=?`;
+  db.all(sql, [username], (err, rows) => {
+    let is = false;
+    if (rows.length == 1) {
+      is = true;
+    }
+    callback(is);
+  });
+}
+
+function login(username, pin, callback) {
+  let sql = `SELECT * FROM users WHERE username=? AND pin=?`;
+  db.all(sql, [username, pin], (err, rows) => {
+    let is = false;
+    if (rows.length == 1) {
+      is = true;
+    }
+    callback(is);
+  });
+}
+
 function saveToJSON(data, filePath) {
   let existingData = {};
 
@@ -71,19 +208,18 @@ function getPosts(data) {
 }
 
 async function main() {
-  await scrapeReddits(["stories", "funnystories"]).then(async (scrapedData) => {
-    const filePath = "data.json";
-
-    // console.log(scrapedData);
-
-    await saveToJSON(scrapedData, filePath);
-
-    const data = await readFromJSON(filePath);
-
-    const posts = getPosts(data);
-
-    console.log(posts);
-  });
+  await scrapeReddits(["StoriesFromYourSchool", "cheating_stories"]).then(
+    (scrapedData) => {
+      const posts = getPosts(scrapedData);
+      for (let i = 0; i < posts.length; i++) {
+        let pid = posts[i].id;
+        let subr = posts[i].reddit;
+        let title = posts[i].title;
+        let text = posts[i].text;
+        addPost(pid, subr, title, text);
+      }
+    }
+  );
 }
 
 // main();
@@ -94,13 +230,108 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/posts", (req, res) => {
-  const data = readFromJSON("data.json");
-  const posts = getPosts(data);
+app.get("/api/posts", (req, res) => {
+  readPosts(0, (posts) => {
+    // console.log(posts);
+    res.send(posts);
+    return;
+  });
+});
 
-  console.log(posts);
-  res.send(posts);
+app.get("/api/posts/approved", (req, res) => {
+  readPosts(1, (posts) => {
+    console.log(posts);
+    res.send(posts);
+    return;
+  });
+});
+
+app.get("/api/posts/picked", (req, res) => {
+  let user = req.header("username");
+  readPicked(user, (posts) => {
+    console.log(posts);
+    res.send(posts);
+    return;
+  });
+});
+
+app.post("/api/posts/approve", (req, res) => {
+  let pid = req.header("pid");
+  let subr = req.header("subr");
+  updatePost("status", 1, pid, subr);
+
+  res.send(200);
   return;
+});
+
+app.post("/api/posts/reject", (req, res) => {
+  let pid = req.header("pid");
+  let subr = req.header("subr");
+  updatePost("status", 2, pid, subr);
+
+  res.send(200);
+  return;
+});
+
+app.post("/api/posts/pick", (req, res) => {
+  let pid = req.header("pid");
+  let subr = req.header("subr");
+  let username = req.header("username");
+  updatePost("status", 4, pid, subr);
+  updatePost("user", username, pid, subr);
+  res.send(200);
+  return;
+});
+
+app.post("/api/posts/finish", (req, res) => {
+  let pid = req.header("pid");
+  let subr = req.header("subr");
+  updatePost("status", 5, pid, subr);
+  res.send(200);
+  return;
+});
+
+app.get("/api/user", (req, res) => {
+  const user = req.header("user");
+  console.log(user);
+  getUser(user, (is) => {
+    console.log(is);
+    if (is) {
+      res.send(true);
+      return;
+    }
+    res.send(false);
+    return;
+  });
+});
+
+app.get("/api/user/login", (req, res) => {
+  const username = req.header("username");
+  const pin = req.header("pin");
+  login(username, pin, (is) => {
+    console.log(is);
+    if (is) {
+      res.send(200);
+      return;
+    }
+    res.send(400);
+    return;
+  });
+});
+
+app.get("/api/user/register", (req, res) => {
+  const username = req.header("username");
+  const pin = req.header("pin");
+  getUser(username, (is) => {
+    if (is) {
+      res.send(400);
+      return;
+    } else {
+      addUser(username, pin);
+      res.send(200);
+      return;
+    }
+  });
 });
 
 app.get("/delete", (req, res) => {
